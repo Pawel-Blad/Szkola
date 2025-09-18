7e3ec7ac1a42d5b6157d249570e7b9973f8f02ce
Pawel-Blad <bladek.gdi@gmail.com>
2025-09-18 16:10:55 +0200

    Add files via upload

---
+++ web/public/skrypt.js
@@ -0,0 +1,10 @@
+function kwadrat() {
+  let kwadrat = document.createElement("div");
+  kwadrat.style.width = "100px";
+  kwadrat.style.height = "100px";
+  kwadrat.style.backgroundColor = "purple";
+
+  document.getElementById("kwadrat").appendChild(kwadrat);
+}
+
+kwadrat();
